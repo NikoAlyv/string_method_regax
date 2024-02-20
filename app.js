e277ea7898c07d0299e313b6e471c0e1a0012ebb
NikoAlyv <enihad801@gmail.com>
2024-02-20 06:19:43 +0400

    string_method

---
+++ app.js
@@ -0,0 +1,29 @@
+
+//TODO Task
+
+const text = "The rain in Spain stays mainly in the plain. In Spain, in April, the rain is a pain."
+
+//! Part 1
+// 
+// const regaxFind =/Spain/
+// const matchEvent = text.match(regaxFind)
+// console.log(matchEvent);
+//! Part 2
+// const regaxAllFind =/rain/g
+// const matchAllEvent =[...text.matchAll(regaxAllFind)]
+// console.log(matchAllEvent)
+
+//! Part 3
+
+// const manipulationCountry = text.replace('Spain','France')
+// console.log(manipulationCountry);
+
+//! Part 4
+
+// const manipulationEvent = text.replaceAll('rain','sun')
+// console.log(manipulationEvent);
+
+//! Part 5
+
+// const positionPlain = text.search('plain')
+// console.log(positionPlain);
